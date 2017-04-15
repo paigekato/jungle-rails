ff8f61ff6b9a5a7ef6f6b866064ca672d898c595
Paige <paigekato@gmail.com>
2017-04-15 19:23:11 +0000

    Went through files to ensure code is DRY and modular. Linted and ensured no dead code.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,14 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+       redirect_to '/'
     else
-      redirect_to '/signup'
+        redirect_to '/signup'
     end
   end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
 end
