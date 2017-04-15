dfe6599da2a142e8970d1fb234546e51f3360eec
Paige <paigekato@gmail.com>
2017-04-15 06:50:52 +0000

    Adding features for styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-    @reviews = Review.where(product_id: params[:id])
+    @reviews = Review.where(product_id: params[:id]).order(created_at: :desc)
   end
 
 end
