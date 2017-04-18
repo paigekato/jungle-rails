07eff1d527f687009c71c60011fb3a0bbbd150df
Paige <paigekato@gmail.com>
2017-04-18 01:36:22 +0000

    User authenticating credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,24 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
   has_many :reviews
-  validates :email, uniqueness: true
+
+  validates_uniqueness_of :email, case_sensitive: false, presence: true
+  validates :password, presence: true
+  validates :password, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+  validates :name, presence: true
+
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'should create valid user' do
+      user = User.create(
+        name: "Paige",
+        email: "paige@paige.com",
+        password: "meow",
+        password_confirmation: "meow"
+
+        )
+      expect(user).to (be_valid)
+    end
+
+    it 'should have password and password confirmation' do
+      user = User.create(
+        name: "Paige",
+        email: "paige@paige.com",
+        password: "meow",
+        password_confirmation: nil
+        )
+      expect(user).to_not (be_valid)
+    end
+
+
+    it 'should not save without unique email' do
+      @user = User.create(
+        name: "Paige",
+        email: "paige@paige.com",
+        password_digest: "me"
+        )
+      @user2 = User.create(
+        name: "Paige",
+        email: "paige@PAIGE.com",
+        password_digest: "me"
+        )
+    expect(@user2).to_not be_valid
+    end
+
+    it 'should have matching password and password confirmation' do
+      password = User.create(
+        name: "Paige",
+        email: "paigekato@gmail.com",
+        password: "meow",
+        password_confirmation: "moo"
+        )
+      expect(password).to_not be_equal(password.password_confirmation)
+    end
+
+    it 'should have name to be a valid user' do
+      user = User.create(
+        name: nil,
+        email: "paige@paige.com",
+        password: "meow",
+        password_confirmation: "meow")
+      expect(user).to_not (be_valid)
+    end
+
+    it 'should have a password with a minimum length of 3' do
+      user = User.create(
+        name: "Paige",
+        email: "paige@paige.com",
+        password: "ah",
+        password_confirmation: "ah"
+        )
+      expect(user).to_not be_valid
+    end
+
+
+
+  end
+end
+
+
+#mut be craeted with password/password_confirmation fields
+#example for where they are not the same
+#also require for creating model so cannot create without valid matching pass
+#emails must be unique and not case senstive
+#email first name and last name are required
+
+  # create_table "users", force: :cascade do |t|
+  #   t.string   "name"
+  #   t.string   "email"
+  #   t.string   "password_digest"
+  #   t.datetime "created_at",      null: false
+  #   t.datetime "updated_at",      null: false
+  #   t.boolean  "email_confirmed"
