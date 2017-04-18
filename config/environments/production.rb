1d731ab945a016dae96fa747460db5123b7dc9c6
Paige <paigekato@gmail.com>
2017-04-18 02:29:23 +0000

    changing config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,6 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-  config.case_insensitive_keys = :email
 
 
   # Prepend all log lines with the following tags.
